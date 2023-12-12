bff8a5d1eeb69a808c94b2a26196ef2ba7010b0b
Dorcas owolabi <owolabidorcas40@gmail.com>
2023-12-12 17:31:33 +0100

    Textfield in my note app

--- app/src/main/java/com/wtm/dorcasnoteapp/MainActivity.kt
+++ app/src/main/java/com/wtm/dorcasnoteapp/MainActivity.kt
@@ -3,36 +3,19 @@ package com.wtm.dorcasnoteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.internal.composableLambda
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.wtm.dorcasnoteapp.Screen.AddNoteScreen
+import com.wtm.dorcasnoteapp.Screen.NoteListScreen
 import com.wtm.dorcasnoteapp.ui.theme.DorcasNoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,121 +29,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen()
+                    AppNavigation()
                 }
             }
         }
     }
 }
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen(){
-    var username by remember {mutableStateOf("")}
-    var password by remember { mutableStateOf("")}
-    var confirmPassword by remember { mutableStateOf("")}
-    var fullName by remember { mutableStateOf("")}
-    var emailAddress by remember { mutableStateOf("")
-
-    }
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.padding(all=8.dp)
+fun AppNavigation(){
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "note-list"
     ){
-        Text(
-            text = "Simple Login Page!",
-            fontSize = 20.sp,
-            textAlign = TextAlign.Center,
-        )
-        TextField(
-            value = fullName,
-            onValueChange = {fullNameInput -> fullName = fullNameInput},
-            label = {Text("Fullname")},
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 10.dp)
-        )
-        TextField(
-            value = emailAddress,
-            onValueChange = {emailAddressInput -> emailAddress = emailAddressInput},
-            label = {Text("EmailAddress")},
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 10.dp)
-        )
-        TextField(
-            value = username,
-            onValueChange = {usernameInput -> username = usernameInput},
-            label = {Text("Username")},
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 10.dp)
-        )
-        TextField(
-            value = password,
-            onValueChange = {pwdInput -> password = pwdInput},
-            label = {Text("Password")},
-            visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 10.dp)
-        )
-
-        TextField(
-            value = confirmPassword,
-            onValueChange = {confirmPasswordInput -> confirmPassword = confirmPasswordInput},
-            label = {Text("confirmPassword")},
-            visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 10.dp)
-
-        )
-       RememberMe()
-        Button(onClick = {}){
-            Text(text="Login")
+        composable("note-list"){
+            NoteListScreen(navController)
         }
-        Row {
-          Image(painter = painterResource(id = R.drawable.google_1),
-              contentDescription = "Clickable facebook icon",
-              modifier = Modifier.size(24.dp)
-          )
-            Image(painter = painterResource(id = R.drawable.facebook_1),
-                contentDescription = "Clickable google icon",
-                 modifier = Modifier
-                     .size(24.dp)
-                     .clickable {
-
-                     }
-
-            )
+        composable("add note"){
+            AddNoteScreen()
         }
     }
-}
-@Composable
-fun RememberMe(){
-    var isChecked by remember{ mutableStateOf(false) }
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Text(text = "Remember Me")
-        Switch(checked = isChecked,
-            onCheckedChange = {isChecked= !isChecked
-            }
-        )
-
-    }
-}
-   @Preview
-   @Composable
-   fun LoginScreenPreview(){
-       Surface(
-           modifier = Modifier.fillMaxSize(),
-           color = MaterialTheme.colorScheme.background
-       ) {
-           LoginScreen()
-       }
 }
